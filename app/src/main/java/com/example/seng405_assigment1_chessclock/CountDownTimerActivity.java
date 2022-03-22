09a033db1116fa811921502c433df0ea9e9040ae
Berke Çınar <brkcnr.16@gmail.com>
2022-03-22 19:39:13 +0300

    CountDownTimer Exercise: activty and xml added

---
+++ app/src/main/java/com/example/seng405_assigment1_chessclock/CountDownTimerActivity.java
@@ -0,0 +1,14 @@
+package com.example.seng405_assigment1_chessclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CountDownTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count_down_timer);
+    }
+}
